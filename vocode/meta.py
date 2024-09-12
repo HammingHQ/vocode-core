c4c0ba2f772604d239f69698e2e3d4087449226b
Marius Buleandra <mbuleandra@gmail.com>
2024-09-11 19:43:09 -0700

    punkt_tab

--- vocode/meta.py
+++ vocode/meta.py
@@ -5,14 +5,11 @@ def ensure_punkt_installed():
     try:
         from nltk.data import find, download
 
-        find("tokenizers/punkt")
-
-        download("punkt")
-        download("punkt_tab")
+        find("tokenizers/punkt_tab")
     except LookupError:
         from nltk import download
 
-        # If not installed, download 'punkt'
-        logger.info("Downloading 'punkt' tokenizer...")
-        download("punkt")
-        logger.info("'punkt' tokenizer downloaded successfully.")
+        # If not installed, download 'punkt_tab'
+        logger.info("Downloading 'punkt_tab' tokenizer...")
+        download("punkt_tab")
+        logger.info("'punkt_tab' tokenizer downloaded successfully.")
